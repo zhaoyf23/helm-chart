915b53a8bd03a63653967e0360949482138982b3
zhaoyf23 <229171662@qq.com>
2021-01-06 15:01:11 +0800

    automatic commit for abc by build.sh at Wed Jan  6 15:01:11 CST 2021

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-function chart_pack() {
-# Go into helm chart directory.
-    cd /root/tmf/helm
+helm_path=/root/tmf/helm-chart
+chart_path=/root/helm/helm-chart
+chart_repo=https://zhaoyf23.github.io/helm-chart/
+
+function check() {
+    # Go into helm chart directory.
+    cd $helm_path
 # check if $@ directory exists, return if not exists
     if [ ! -d $@ ]; then
         echo "warning: "$@ not exists, continue for scanning other directory...
@@ -10,33 +14,38 @@ function chart_pack() {
     fi
 # Delete old chart
     rm -rf $@-*.tgz
+}
+
+function source_commit() {
+    cd $helm_path
+    git checkout main
+    git add --all . && git commit -m "automatic commit for $@ by build.sh at `date`" && git push
+}
+
+function chart_pack() {
+    cd $helm_path
 # Packages a chart into a versioned chart archive file.
     helm package $@
 # Go into helm chart archive file directory, delete old cart archive file.
-    cd /root/helm/helm-chart && rm -rf $@-*.tgz
+    cd $chart_path && rm -rf $@-*.tgz
 # Git checkout branch of chart repository.
     git checkout gh-pages
-    mv /root/tmf/helm/$@-*.tgz .
+    mv $helm_path/$@-*.tgz .
 }
 
 function chart_commit() {
-# Go into helm chart directory.
-    cd /root/tmf/helm
-# check if $@ directory exists, return if not exists
-    if [ ! -d $@ ]; then
-        echo "warning: "$@ not exists, continue for scanning other directory...
-        return;
-    fi
 # Go into helm chart archive file directory, delete old index.yaml file.
-    cd /root/helm/helm-chart && rm -rf index.yaml
+    cd $chart_path && rm -rf index.yaml
 # Read the current directory and generate an index file based on the charts found.
-    helm repo index --url https://zhaoyf23.github.io/helm-chart/ --merge index.yaml .
+    helm repo index --url $chart_repo --merge index.yaml .
 # Commit current directory to branch of chart repository.
     git add --all . && git commit -m "automatic commit for $@ by build.sh at `date`" && git push
 }
 
 for arg in $*
 do
+  check $arg
+  source_commit $arg
   chart_pack $arg
   chart_commit $arg
 done
